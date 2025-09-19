b20809e77e702da61588606c157554d5ed261c21
Besher Kitaz <kitazb@berea.edu>
2025-09-19 12:54:16 -0400

    AI connect works now and cards are being generated sucessfully from photos

--- server/app/logic/generate_cards_from_photo.py
+++ server/app/logic/generate_cards_from_photo.py
@@ -1,15 +1,26 @@
-import google.generativeai as genai
+from google import genai
+from google.genai import types
+import requests
 from app.secret_key import GOOGLE_API_KEY
+import json
+import re
 
 
 
-genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
 
 def generate_flashcards_from_photo(file):
-    image_part = genai.Part(
-        mime_type=file.content_type,
-        data=file.read()
+    """
+    Generate flashcards from a photo file using Google Gemini API.
+    Args:
+        file: A file-like object containing the image.
+    :returns:
+        A list of flashcards (dicts), where each flashcard is a dictionary with 'front' and 'back' keys.
+    """
+    image_bytes = file.read()  
+    file.seek(0)  # reset pointer so you can use file again if needed
+
+    image = types.Part.from_bytes(
+    data=image_bytes, mime_type="image/jpeg"
     )
     prompt = (
     "Generate flashcards from this photo. The response should be a JSON array "
@@ -26,7 +37,13 @@ def generate_flashcards_from_photo(file):
     "  }\n"
     "]\n\n"
 )
-    response = model.generate_content([prompt, image_part])
-    print(response.text)
-    return response.text
+    
+    client = genai.Client(api_key=GOOGLE_API_KEY)
+    response = client.models.generate_content(
+        model='gemini-1.5-flash',
+        contents= [prompt, image],
+    )
+    processed_text = re.sub(r"^```json\s*|\s*```$", "", response.text.strip(), flags=re.DOTALL).strip()
+    cards = json.loads(processed_text)
+    return cards
 
